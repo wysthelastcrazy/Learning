c533879692032379955ece8ac7240ab0f92e9c45
wangyasheng <wangyasheng@aixuexi.com>
2021-03-10 18:51:56 +0800

    剑指 Offer 32 - II. 剑指 Offer 32 - III.

--- leetcode/src/main/java/com/wys/offer/tree/Offer32_levelOrder.java
+++ leetcode/src/main/java/com/wys/offer/tree/Offer32_levelOrder.java
@@ -1,9 +1,12 @@
 package com.wys.offer.tree;
 
+import com.sun.tools.corba.se.idl.InterfaceGen;
 import com.wys.offer.tree.node.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -67,6 +70,75 @@ public class Offer32_levelOrder {
         int[] res = new int[ans.size()];
         for(int i = 0; i < ans.size(); i++){
             res[i] = ans.get(i);
+        }
+        return res;
+    }
+
+    /**
+     * 面试题32 - II. 从上到下打印二叉树 II
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，
+     * 每一层打印到一行。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root){
+        if (root == null)
+            return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        while (!queue.isEmpty()){
+            List<Integer> tmp = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size ;i++){
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    /**
+     * 剑指 Offer 32 - III. 从上到下打印二叉树 III
+     * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，
+     * 第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder3(TreeNode root){
+        if (root == null)
+            return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        while (!queue.isEmpty()){
+            List<Integer> tmp = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size ;i++){
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            if (res.size() % 2 == 1){
+                Collections.reverse(tmp);
+            }
+            res.add(tmp);
         }
         return res;
     }
