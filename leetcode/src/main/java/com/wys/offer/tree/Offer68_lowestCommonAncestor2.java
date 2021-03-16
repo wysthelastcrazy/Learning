419af68f4434e4d0dea7a10e121d551ca8b708ac
wangyasheng <wangyasheng@aixuexi.com>
2021-03-16 12:36:25 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先

---
+++ leetcode/src/main/java/com/wys/offer/tree/Offer68_lowestCommonAncestor2.java
@@ -0,0 +1,32 @@
+package com.wys.offer.tree;
+
+import com.wys.offer.tree.node.TreeNode;
+
+/**
+ * @author wangyasheng
+ * @date 2021-03-16
+ * @describe:剑指 Offer 68 - II. 二叉树的最近公共祖先
+ *
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Offer68_lowestCommonAncestor2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if (left == null)
+            return right;
+        if (right == null)
+            return left;
+        return root;
+    }
+}
